23a4cc3ca0bd50cc2c784f49cf535bcf1e96b1b2
Karl Digno <dazzlejayberocil07@gmail.com>
2026-09-09 19:13:57 +0800

    Lab5

---
+++ app/controllers/Crudcontroller.php
@@ -0,0 +1,14 @@
+<?php
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+
+/**
+ * Controller: Crudcontroller
+ * 
+ * Automatically generated via CLI.
+ */
+class Crudcontroller extends Controller {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
